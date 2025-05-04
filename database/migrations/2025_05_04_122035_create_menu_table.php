df5e798c38ca9091d810ed1c5fe9da4a93676cd1
DESKTOP-B8GOP7R\HP <gps.traceme365.01@gmail.com>
2025-05-04 17:56:01 +0530

    Menu Migration

---
+++ database/migrations/2025_05_04_122035_create_menu_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tms_menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('tms_menu_icon');
+            $table->string('tms_menu_name');
+            $table->string('tms_menu_route');
+            $table->integer('tms_menu_order');
+            $table->string('tms_menu_package');
+            $table->integer('tms_menu_parent');
+            $table->integer('tms_menu_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu');
+    }
+};
